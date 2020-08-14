e52efbfce02460a17b1bccce7e6f9d8dc2ce03c4
Aleksey Kurnosenko <lekz112@gmail.com>
2020-08-14 20:12:59 +0200

    Have profile extends AbstractEntity

--- src/main/kotlin/com/delivery/demo/profile/Profile.kt
+++ src/main/kotlin/com/delivery/demo/profile/Profile.kt
@@ -1,19 +1,15 @@
 package com.delivery.demo.profile
 
+import com.delivery.demo.AbstractEntity
 import com.delivery.demo.Address
 import org.hibernate.annotations.Where
-
-import java.util.*
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "profiles")
 @Where(clause = "is_deleted = false")
-data class Profile(
-    @Id val id: UUID = UUID.randomUUID()
-) {
+class Profile : AbstractEntity() {
     var userId: String? = null
     var deliveryAddress: Address? = null
     var paymentMethodId: String? = null

b718682592504d01bec63b63ae594246e7834223
Aleksey Kurnosenko <lekz112@gmail.com>
2020-08-14 13:43:14 +0200

    Remove unused query from Repository. Re-enable schema autocreation.

--- src/main/kotlin/com/delivery/demo/order/OrderRepository.kt
+++ src/main/kotlin/com/delivery/demo/order/OrderRepository.kt
@@ -7,7 +7,6 @@ import java.util.*
 
 interface OrderRepository : CrudRepository<Order, UUID> {
     fun findAll(pageable: Pageable): Page<Order>
-    fun findAllByOrderByCreatedDateDesc(): List<Order>
     fun findByRestaurantId(restaurantId: UUID): List<Order>
     fun findByStatusAndRestaurantId(status: OrderStatus, restaurantId: UUID): List<Order>
 }
